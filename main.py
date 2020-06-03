9a0f0bf4eb4d657e34c62adab6bc2ec7c12ab4c2
Fabien Collet <fbncollet@gmail.com>
2020-06-02 22:02:25 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#title           :pyscript.py
+#description     :This will create a header for a python script.
+#author          :bgw
+#date            :20110930
+#version         :0.4
+#usage           :python pyscript.py
+#notes           :
+#python_version  :2.6.6
+# ==============================================================================
